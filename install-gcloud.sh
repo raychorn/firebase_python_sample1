f43a5f45ebc9170cf39b7dbd666af6e214575614
Ray C Horn <raychorn@gmail.com>
2020-10-29 21:35:17 +0000

    one more test.

--- install-gcloud.sh
+++ install-gcloud.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/bash
+#! /usr/bin/bash
 
 cp install-gcloud-sdk.exp /root/google-cloud-sdk/install-gcloud-sdk.exp
+cd /root/google-cloud-sdk
 /root/google-cloud-sdk/install-gcloud-sdk.exp
 . /root/.bashrc
